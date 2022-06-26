f83dbb46460637c5b42756bf00edc8dad410739a
David Martinez <anddagger@gmail.com>
2022-06-26 12:02:09 -0500

    added my bio

--- components/theme-toggle-button.js
+++ components/theme-toggle-button.js
@@ -7,12 +7,22 @@ const ThemeToggleButton = () => {
 const {toggleColorMode} =useColorMode()
 
 return(
+  <AnimatePresence exitBeforeEnter initial={false}>
+    <motion.div style={{display:'inline-block'}}
+    key={useColorMode('light','dark')}
+    initial={{y:-20,opacity:0}}
+    animate={{y:0,opacity:0}}
+    exit={{y:20,opacity:0}}
+    transition={{duration:0.2}}
+    >
+    </motion.div>
   <IconButton aria-label='Toggle theme'
   colorScheme={useColorModeValue('purple','orange')}
   icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
   onClick={toggleColorMode}>
 
   </IconButton>
+  </AnimatePresence>
 )
 }
 
