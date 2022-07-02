8b1bfe4f1a05e0e661f7ab0eb0baf5ab69a9c099
David Martinez <anddagger@gmail.com>
2022-07-01 20:13:07 -0500

    add norefferer attr to footer tags to address security risk with older browsers

--- components/footer.js
+++ components/footer.js
@@ -5,16 +5,27 @@ const Footer = () => {
     <Box align="center" opacity={0.4} fontSize="sm">
       &copy; {new Date().getFullYear()} Marti McDee, LLC
       <br />
-      <a href="https://sketchfab.com/3d-models/shiba-faef9fe5ace445e7b2989d1c1ece361c" target={"_blank"}>
+      <a
+        href="https://sketchfab.com/3d-models/shiba-faef9fe5ace445e7b2989d1c1ece361c"
+        target={'_blank'}
+        rel="noreferrer"
+      >
         &quot;Shiba&quot;
       </a>
-      , by <a href="https://sketchfab.com/zixisun51" target={"_blank"}>zlxlsun02</a> licensed
-      under{' '}
+      , by{' '}
+      <a
+        href="https://sketchfab.com/zixisun51"
+        target={'_blank'}
+        rel="noreferrer"
+      >
+        zlxlsun02
+      </a>{' '}
+      licensed under{' '}
       <a
         about="/public/assets/shiba/scene.gltf"
-        rel="license"
+        rel="license noreferrer"
         href="https://creativecommons.org/licenses/by/4.0/"
-        target={"_blank"}
+        target={'_blank'}
       >
         CC BY 4.0
       </a>
