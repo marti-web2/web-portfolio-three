9b86c84f203f29189433ec6fcd3f292b8f3f254f
David Martinez <anddagger@gmail.com>
2022-07-09 16:25:08 -0500

    increased font size and iorted font family from Gooogle fonts

--- pages/_document.js
+++ pages/_document.js
@@ -2,12 +2,25 @@ import { ColorModeScript } from '@chakra-ui/react'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 import theme from '../lib/theme'
 
+const rootStyle = {
+ fontSize: '10px',
+}
+
+const bodyStyle = {
+  fontSize: '1.8rem'
+}
+
 export default class Document extends NextDocument {
   render() {
     return (
-      <Html lang="en">
-        <Head />
-        <body>
+      <Html lang="en" style={rootStyle}>
+        <Head>
+        <link
+  href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
+  rel="stylesheet" />
+
+</Head>
+        <body style={bodyStyle}>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
--- pages/works/delicious-dining.js
+++ pages/works/delicious-dining.js
@@ -1,30 +1,41 @@
 import {
   Container,
   Badge,
+  Flex,
+
+  Image,
   Link,
   List,
-  ListItem
+  ListItem, 
+  Text
 } from '@chakra-ui/react'
 import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { Title, WorkImage, Meta } from '../../components/work'
 import P from '../../components/paragraph'
 import Layout from '../../components/layouts/article'
 
+
 const Work = () => (
   <Layout title="Delicious Dining">
     <Container>
       <Title>
-        Delicious Dining <Badge>2016-</Badge>
+        Delicious Dining <Badge>2022</Badge>
       </Title>
       <P>
-        A Markdown note-taking app with 100+ plugins, cross-platform and
-        encrypted data sync support. The life-time revenue is more than $300k.
+        A useful app with detailed instructions to help anyone prepare a delicious gourmet meal. The Splide component
+        is implemented to give the user a smooth experience in selecting recipes from a handy carousel.
       </P>
       <List ml={4} my={4}>
         <ListItem>
           <Meta>Website</Meta>
-          <Link href="https://delicious-dining.online/">
-          https://delicious-dining.online/ <ExternalLinkIcon mx="2px" />
+          <Link href="https://delicious-dining.online/" target="_blank">
+            https://delicious-dining.online/ <ExternalLinkIcon mx="2px" />
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Meta>Source</Meta>
+          <Link href="https://github.com/marti-web2/delicious-recipes-react" target="_blank">
+            {'<> Code'} <ExternalLinkIcon mx="2px" />
           </Link>
         </ListItem>
         <ListItem>
@@ -32,15 +43,23 @@ const Work = () => (
           <span>Web 2.0</span>
         </ListItem>
         <ListItem>
-          <Meta>Stack</Meta>
-          <span>React, NodeJs</span>
+          <Flex align="left">
+            <Meta>Stack</Meta>
+            <Image src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" alt='React Logo' 
+            boxSize={'1.75rem'} borderRadius='full' ignoreFallback /><Text ml={2}>React</Text>
+
+          </Flex>
         </ListItem>
       </List>
       <WorkImage src="/images/works/delicious-recipes_01.png" alt="Delicious Dining" />
       <WorkImage src="/images/works/delicious-recipes_02.png" alt="Delicious Dining" />
     </Container>
+
   </Layout>
 )
 
 export default Work
 export { getServerSideProps } from '../../components/chakra'
+
+
+
