7f0d568d1c7482b1223afdfb684f9e7317531324
Marti <anddagger@gmail.com>
2022-08-13 23:13:20 +0000

    added 'creatures' to Works page

--- pages/works.js
+++ pages/works.js
@@ -2,7 +2,8 @@ import { Container, Divider, Heading, SimpleGrid } from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
-import thumbDining from '../public/images/works/delicious-recipes_card.png'
+import thumbDining from '../public/images/works/delicious-dining/delicious-recipes_card.png'
+import thumbCreatures from '../public/images/works/creatures-of-the-night/creatures_card.png'
 
 const Works = () => (
   <Layout title="Works">
@@ -18,7 +19,19 @@ const Works = () => (
             title="Delicious Dining"
             thumbnail={thumbDining}
           >
-           An app that provides detailed instructions to help anyone prepare a delicious gourmet meal.
+            An app that provides detailed instructions to help anyone prepare a
+            delicious gourmet meal.
+          </WorkGridItem>
+        </Section>
+        <Section delay={0.2}>
+          <Divider my={6} />
+          <WorkGridItem
+            id="creatures-of-the-night"
+            title="Creatures of the Night"
+            thumbnail={thumbCreatures}
+          >
+            A side scrolling arcade-style action game. The goal is to defeat
+            enough night creatures before the timer expires.
           </WorkGridItem>
         </Section>
       </SimpleGrid>
--- pages/works/creatures-of-the-night.js
+++ pages/works/creatures-of-the-night.js
@@ -14,27 +14,29 @@ import P from '../../components/paragraph'
 import Layout from '../../components/layouts/article'
 
 const Work = () => (
-  <Layout title="Delicious Dining">
+  <Layout title="Creatures of the Night">
     <Container>
       <Title>
-        Delicious Dining <Badge>2022</Badge>
+        Creatures of the Night - <em>Alpha</em> <Badge>2022</Badge>
       </Title>
       <P>
-        An app that provides detailed instructions to help anyone prepare a
-        delicious gourmet meal. The Splide component is implemented to give the
-        user a smooth experience in selecting recipes from a handy carousel.
+        Blackjack is a curious little furry adventurer who wandered off too far
+        from his normal route on his morning walk. Help our friend find his way
+        back home to his lovely wife Undgretta before the sun goes down and he
+        finds himself spending the night outdoors amongst the creepy-crawlies of
+        the forest.
       </P>
       <List ml={4} my={4}>
         <ListItem>
           <Meta>Website</Meta>
-          <Link href="https://delicious-dining.online/" target="_blank">
-            https://delicious-dining.online/ <ExternalLinkIcon mx="2px" />
+          <Link href="https://marti-web2.github.io/" target="_blank">
+            https://marti-web2.github.io/ <ExternalLinkIcon mx="2px" />
           </Link>
         </ListItem>
         <ListItem>
           <Meta>Source</Meta>
           <Link
-            href="https://github.com/marti-web2/delicious-recipes-react"
+            href="https://github.com/marti-web2/creatures-of-the-night"
             target="_blank"
           >
             {'<> Code'} <ExternalLinkIcon mx="2px" />
@@ -57,33 +59,33 @@ const Work = () => (
           <Flex mt={1}>
             <Meta>Stack</Meta>
             <Image
-              src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
-              alt="React Logo"
+              src="https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg"
+              alt="TypeScript Logo"
               boxSize={'1.5rem'}
               borderRadius="full"
               ignoreFallback
             />
             <Text ml={2} mr={4}>
-              React
+              TypeScript
             </Text>
             <Image
-              src="../../images/icons/node.png"
-              alt="Node Logo"
+              src="https://upload.wikimedia.org/wikipedia/commons/9/96/Sass_Logo_Color.svg"
+              alt="Sass Logo"
               boxSize={'1.5rem'}
               borderRadius="full"
               ignoreFallback
             />
-            <Text ml={2}>Node</Text>
+            <Text ml={2}>Sass</Text>
           </Flex>
         </ListItem>
       </List>
       <WorkImage
-        src="/images/works/delicious-dining/delicious-recipes_01.png"
-        alt="Photo of Home Page"
+        src="/images/works/creatures-of-the-night/creatures_01.png"
+        alt="Photo of Gameplay"
       />
       <WorkImage
-        src="/images/works/delicious-dining/delicious-recipes_02.png"
-        alt="Photo of Recipe Page"
+        src="/images/works/creatures-of-the-night/creatures_02.png"
+        alt="Photo of Game Over Screen"
       />
     </Container>
   </Layout>
