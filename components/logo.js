002fa32029e5d897b7e935e632d2feb98722362d
David Martinez <anddagger@gmail.com>
2022-12-18 17:46:46 -0600

    logo change

--- components/logo.js
+++ components/logo.js
@@ -32,7 +32,7 @@ const Logo = () => {
             fontWeight={'bold'}
             ml={3}
           >
-            Marti McDee
+            Marti Martinez
           </Text>
         </LogoBox>
       </a>
